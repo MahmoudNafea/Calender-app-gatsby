b7254b6aae9957445f5e36c789fc81e204462b07
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-12-07 22:38:48 +0800

    init the app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
+import { Link } from 'gatsby'
+import '../styles/index.css'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Calendar App</h1>
+            <div>
+                <p>Please login to continue</p>
+                <Link to='/calendar'>Bob</Link>
+                <Link to='/calendar'>Robert</Link>
+            </div>
+        </div>
+    )
+}
+
+export default IndexPage
