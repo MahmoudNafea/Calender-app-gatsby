d3c645ac3fdda52f0f98fe7d054d1455344b3547
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2019-12-08 16:51:06 +0800

    login validation

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,11 +1,27 @@
 import React from "react"
+import Calendar from 'react-calendar'
+import Popup from "reactjs-popup";
+
 
 const IndexPage = () => {
+    const clicked = () => {
+        return (
+            <Popup position="right center">
+                <div>Popup content here !!</div>
+            </Popup>
+        )
+    }
+
     return (
         <div>
             <h1>Tasks</h1>
+            <Calendar onClickDay={clicked} />
+            <Popup position="right center">
+                <div>Popup content here !!</div>
+            </Popup>
         </div>
     )
 }
+
 
 export default IndexPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,37 @@
-import React from "react"
-import { Link } from 'gatsby'
+import React, { useState } from "react"
 import '../styles/index.scss'
 
+
+
 const IndexPage = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const userData = { username, password }
+
+    const loginHandler = (e) => {
+        e.preventDefault()
+
+        const usersList = ['bob', 'robert']
+        if (usersList.includes(username.toLowerCase())) {
+            console.log(username)
+        }
+    }
+
     return (
         <div>
             <h1>Calendar App</h1>
             <div>
                 <p>Please login to continue</p>
-                <Link to='/calendar'>Bob</Link>
-                <Link to='/calendar'>Robert</Link>
+                <form onSubmit={loginHandler}>
+                    <label>Username</label>
+                    <input type="text" placeholder="Only as Bob or Robert"
+                        value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <label>Password</label>
+                    <input type="password" placeholder="Password"
+                        value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit" >Login</button>
+                </form>
             </div>
         </div>
     )
